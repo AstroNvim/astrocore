18e9189574ecae363074c7d23f9729cdf7f3818f
Micah Halter <micah@mehalter.com>
2023-07-21 08:21:09 -0400

    refactor!: icons moved to AstroUI

--- lua/astrocore/utils.lua
+++ lua/astrocore/utils.lua
@@ -79,34 +79,6 @@ function M.conditional_func(func, condition, ...)
   if condition and type(func) == "function" then return func(...) end
 end
 
---- Get an icon from the AstroNvim internal icons if it is available and return it
----@param kind string The kind of icon in astronvim.icons to retrieve
----@param padding? integer Padding to add to the end of the icon
----@param no_fallback? boolean Whether or not to disable fallback to text icon
----@return string icon
-function M.get_icon(kind, padding, no_fallback)
-  if not vim.g.icons_enabled and no_fallback then return "" end
-  local icon_pack = vim.g.icons_enabled and "icons" or "text_icons"
-  if not M[icon_pack] then
-    M.icons = astronvim.user_opts("icons", require "astronvim.icons.nerd_font")
-    M.text_icons = astronvim.user_opts("text_icons", require "astronvim.icons.text")
-  end
-  local icon = M[icon_pack] and M[icon_pack][kind]
-  return icon and icon .. string.rep(" ", padding or 0) or ""
-end
-
---- Get a icon spinner table if it is available in the AstroNvim icons. Icons in format `kind1`,`kind2`, `kind3`, ...
----@param kind string The kind of icon to check for sequential entries of
----@return string[]|nil spinners # A collected table of spinning icons in sequential order or nil if none exist
-function M.get_spinner(kind, ...)
-  local spinner = {}
-  repeat
-    local icon = M.get_icon(("%s%d"):format(kind, #spinner + 1), ...)
-    if icon ~= "" then table.insert(spinner, icon) end
-  until not icon or icon == ""
-  if #spinner > 0 then return spinner end
-end
-
 --- Get highlight properties for a given highlight name
 ---@param name string The highlight group name
 ---@param fallback? table The fallback highlight properties
