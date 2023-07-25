a26729d082123c65d4dff4fbe895a1e8cf719d3a
Micah Halter <micah@mehalter.com>
2023-07-25 13:18:29 -0400

    refactor!: remove astronvim updater and git utilities

--- lua/astrocore/utils.lua
+++ lua/astrocore/utils.lua
@@ -313,4 +313,30 @@ function M.cmd(cmd, show_error)
   return success and result:gsub("[\27\155][][()#;?%d]*[A-PRZcf-ntqry=><~]", "") or nil
 end
 
+--- Get the first worktree that a file belongs to
+---@param file string? the file to check, defaults to the current file
+---@param worktrees table<string, string>[]? an array like table of worktrees with entries `toplevel` and `gitdir`, default retrieves from `vim.g.git_worktrees`
+---@return table<string, string>|nil # a table specifying the `toplevel` and `gitdir` of a worktree or nil if not found
+function M.file_worktree(file, worktrees)
+  worktrees = worktrees or require("astrocore").config.git_worktrees
+  if not worktrees then return end
+  file = file or vim.fn.expand "%"
+  for _, worktree in ipairs(worktrees) do
+    if
+      M.cmd({
+        "git",
+        "--work-tree",
+        worktree.toplevel,
+        "--git-dir",
+        worktree.gitdir,
+        "ls-files",
+        "--error-unmatch",
+        file,
+      }, false)
+    then
+      return worktree
+    end
+  end
+end
+
 return M
