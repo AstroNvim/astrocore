814c1ef75f9c2d8de99c5885bdda6446f7e00837
Micah Halter <micah@mehalter.com>
2023-07-31 20:14:50 -0400

    refactor: update all mappings to match casing in vim docs (`:h`)

--- lua/astrocore/init.lua
+++ lua/astrocore/init.lua
@@ -141,8 +141,8 @@ end
 ---@param txt string The explanation text of what the keybinding does
 ---@return table # A button entity table for an alpha configuration
 function M.alpha_button(sc, txt)
-  -- replace <leader> in shortcut text with LDR for nicer printing
-  local sc_ = sc:gsub("%s", ""):gsub("LDR", "<leader>")
+  -- replace <Leader> in shortcut text with LDR for nicer printing
+  local sc_ = sc:gsub("%s", ""):gsub("LDR", "<Leader>")
   -- if the leader is set, replace the text with the actual leader key for nicer printing
   if vim.g.mapleader then sc = sc:gsub("LDR", vim.g.mapleader == " " and "SPC" or vim.g.mapleader) end
   -- return the button entity to display the correct text and send the correct keybinding on press
