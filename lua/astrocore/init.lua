484de6051cfd9ffdb53fd967397d80836fcb846e
Micah Halter <micah@mehalter.com>
2023-08-15 07:43:57 -0400

    refactor!: remove `alpha_button` function since alpha provides it's own

--- lua/astrocore/init.lua
+++ lua/astrocore/init.lua
@@ -139,36 +139,6 @@ function M.toggle_term_cmd(opts)
   terms[opts.cmd][num]:toggle()
 end
 
---- Create a button entity to use with the alpha dashboard
----@param sc string The keybinding string to convert to a button
----@param txt string The explanation text of what the keybinding does
----@return table # A button entity table for an alpha configuration
-function M.alpha_button(sc, txt)
-  -- replace <Leader> in shortcut text with LDR for nicer printing
-  local sc_ = sc:gsub("%s", ""):gsub("LDR", "<Leader>")
-  -- if the leader is set, replace the text with the actual leader key for nicer printing
-  if vim.g.mapleader then sc = sc:gsub("LDR", vim.g.mapleader == " " and "SPC" or vim.g.mapleader) end
-  -- return the button entity to display the correct text and send the correct keybinding on press
-  return {
-    type = "button",
-    val = txt,
-    on_press = function()
-      local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
-      vim.api.nvim_feedkeys(key, "normal", false)
-    end,
-    opts = {
-      position = "center",
-      text = txt,
-      shortcut = sc,
-      cursor = -2,
-      width = 36,
-      align_shortcut = "right",
-      hl = "DashboardCenter",
-      hl_shortcut = "DashboardShortcut",
-    },
-  }
-end
-
 --- Get a plugin spec from lazy
 ---@param plugin string The plugin to search for
 ---@return LazyPlugin? available # The found plugin spec from Lazy
