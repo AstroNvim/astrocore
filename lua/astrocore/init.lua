4e677ff984f099832041feb6b7d304b1388f2ff6
Micah Halter <micah@mehalter.com>
2023-08-19 06:30:48 -0400

    perf: remove need to make deep copies

--- lua/astrocore/init.lua
+++ lua/astrocore/init.lua
@@ -337,18 +337,19 @@ function M.setup(opts)
     local augroup_id = vim.api.nvim_create_augroup(augroup, { clear = true })
     for _, autocmd in ipairs(autocmds) do
       local event = autocmd.event
-      local final_autocmd = vim.deepcopy(autocmd)
-      final_autocmd.event, final_autocmd.group = nil, augroup_id
-      vim.api.nvim_create_autocmd(event, final_autocmd)
+      autocmd.event = nil
+      autocmd.group = augroup_id
+      vim.api.nvim_create_autocmd(event, autocmd)
+      autocmd.event = event
     end
   end
 
   -- user commands
   for cmd, spec in pairs(M.config.commands) do
     local action = spec[1]
-    local final_spec = vim.deepcopy(spec)
-    final_spec[1] = nil
-    vim.api.nvim_create_user_command(cmd, action, final_spec)
+    spec[1] = nil
+    vim.api.nvim_create_user_command(cmd, action, spec)
+    spec[1] = action
   end
 
   -- on_key hooks
