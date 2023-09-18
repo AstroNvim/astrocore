1a1bff7da93497ea9751a4dc71cfadb99ddc4804
Micah Halter <micah@mehalter.com>
2023-09-18 06:19:52 -0400

    fix(resession): only change buffers on load if cursor is in a wipe buffer

--- lua/resession/extensions/astrocore.lua
+++ lua/resession/extensions/astrocore.lua
@@ -38,7 +38,9 @@ M.on_load = function(data)
 
   require("astrocore").event "BufsUpdated"
 
-  if vim.fn.bufnr() ~= buf_utils.current_buf then vim.cmd.b(buf_utils.current_buf) end
+  if vim.opt.bufhidden:get() == "wipe" and vim.fn.bufnr() ~= buf_utils.current_buf then
+    vim.cmd.b(buf_utils.current_buf)
+  end
 end
 
 return M
