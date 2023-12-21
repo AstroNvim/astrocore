ca29e21c1a5ffa210fa39336126f9591f9966c5c
Micah Halter <micah@mehalter.com>
2023-12-21 13:03:06 -0500

    feat: add ability for `on_load` to easily just load another plugin

--- lua/astrocore/init.lua
+++ lua/astrocore/init.lua
@@ -178,14 +178,19 @@ end
 
 --- Execute a function when a specified plugin is loaded with Lazy.nvim, or immediately if already loaded
 ---@param plugins string|string[] the name of the plugin or a list of plugins to defer the function execution on. If a list is provided, only one needs to be loaded to execute the provided function
----@param func fun() the function to execute when the plugin is loaded
-function M.on_load(plugins, func)
-  if type(plugins) == "string" then plugins = { plugins } end
+---@param load_op fun()|string|string[] the function to execute when the plugin is loaded, a plugin name to load, or a list of plugin names to load
+function M.on_load(plugins, load_op)
   local lazy_config_avail, lazy_config = pcall(require, "lazy.core.config")
   if lazy_config_avail then
+    if type(plugins) == "string" then plugins = { plugins } end
+    if type(load_op) ~= "function" then
+      local to_load = type(load_op) == "string" and { load_op } or load_op --[=[@as string[]]=]
+      load_op = function() require("lazy").load { plugins = to_load } end
+    end
+
     for _, plugin in ipairs(plugins) do
       if vim.tbl_get(lazy_config.plugins, plugin, "_", "loaded") then
-        vim.schedule(func)
+        vim.schedule(load_op)
         return
       end
     end
@@ -195,7 +200,7 @@ function M.on_load(plugins, func)
       desc = ("A function to be ran when one of these plugins runs: %s"):format(vim.inspect(plugins)),
       callback = function(args)
         if vim.tbl_contains(plugins, args.data) then
-          func()
+          load_op()
           if autocmd then vim.api.nvim_del_autocmd(autocmd) end
         end
       end,
