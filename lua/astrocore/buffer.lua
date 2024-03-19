685da236d8649fb6c259ec8b49c5f74b84020cc9
Micah Halter <micah@mehalter.com>
2024-03-19 08:43:56 -0400

    feat(buffer): add `wipe` function to fully wipe a buffer

--- lua/astrocore/buffer.lua
+++ lua/astrocore/buffer.lua
@@ -121,30 +121,52 @@ function M.prev()
   end
 end
 
+--- Helper function to power a save confirmation prompt before `mini.bufremove`
+---@param func fun(bufnr:integer,force:boolean?) The function to execute if confirmation is passed
+---@param bufnr integer The buffer to close or the current buffer if not provided
+---@param force? boolean Whether or not to foce close the buffers or confirm changes (default: false)
+---@return boolean? # new value for whether to force save, `nil` to skip saving
+local function mini_confirm(func, bufnr, force)
+  if not force and vim.bo[bufnr].modified then
+    local bufname = vim.fn.expand "%"
+    local empty = bufname == ""
+    if empty then bufname = "Untitled" end
+    local confirm = vim.fn.confirm(('Save changes to "%s"?'):format(bufname), "&Yes\n&No\n&Cancel", 1, "Question")
+    if confirm == 1 then
+      if empty then return end
+      vim.cmd.write()
+    elseif confirm == 2 then
+      force = true
+    else
+      return
+    end
+  end
+  func(bufnr, force)
+end
+
 --- Close a given buffer
 ---@param bufnr? integer The buffer to close or the current buffer if not provided
 ---@param force? boolean Whether or not to foce close the buffers or confirm changes (default: false)
 function M.close(bufnr, force)
   if not bufnr or bufnr == 0 then bufnr = vim.api.nvim_get_current_buf() end
   if astro.is_available "mini.bufremove" and M.is_valid(bufnr) and #vim.t.bufs > 1 then
-    if not force and vim.bo[bufnr].modified then
-      local bufname = vim.fn.expand "%"
-      local empty = bufname == ""
-      if empty then bufname = "Untitled" end
-      local confirm = vim.fn.confirm(('Save changes to "%s"?'):format(bufname), "&Yes\n&No\n&Cancel", 1, "Question")
-      if confirm == 1 then
-        if empty then return end
-        vim.cmd.write()
-      elseif confirm == 2 then
-        force = true
-      else
-        return
-      end
-    end
-    require("mini.bufremove").delete(bufnr, force)
+    mini_confirm(require("mini.bufremove").delete, bufnr, force)
   else
     local buftype = vim.bo[bufnr].buftype
     vim.cmd(("silent! %s %d"):format((force or buftype == "terminal") and "bdelete!" or "confirm bdelete", bufnr))
+  end
+end
+
+--- Fully wipeout a given buffer
+---@param bufnr? integer The buffer to wipe or the current buffer if not provided
+---@param force? boolean Whether or not to foce close the buffers or confirm changes (default: false)
+function M.wipe(bufnr, force)
+  if not bufnr or bufnr == 0 then bufnr = vim.api.nvim_get_current_buf() end
+  if astro.is_available "mini.bufremove" and M.is_valid(bufnr) and #vim.t.bufs > 1 then
+    mini_confirm(require("mini.bufremove").wipeout, bufnr, force)
+  else
+    local buftype = vim.bo[bufnr].buftype
+    vim.cmd(("silent! %s %d"):format((force or buftype == "terminal") and "bwipeout!" or "confirm bwipeout", bufnr))
   end
 end
 
