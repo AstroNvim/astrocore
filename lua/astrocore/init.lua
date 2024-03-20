08f73cee62e38793085c631f32036ff76e3c9c50
Micah Halter <micah@mehalter.com>
2024-03-20 08:14:44 -0400

    fix: extend `on_exit` passed to custom toggle terminal

--- lua/astrocore/init.lua
+++ lua/astrocore/init.lua
@@ -135,7 +135,11 @@ function M.toggle_term_cmd(opts)
   if not terms[opts.cmd] then terms[opts.cmd] = {} end
   if not terms[opts.cmd][num] then
     if not opts.count then opts.count = vim.tbl_count(terms) * 100 + num end
-    if not opts.on_exit then opts.on_exit = function() terms[opts.cmd][num] = nil end end
+    local on_exit = opts.on_exit
+    opts.on_exit = function(...)
+      terms[opts.cmd][num] = nil
+      if on_exit then on_exit(...) end
+    end
     terms[opts.cmd][num] = require("toggleterm.terminal").Terminal:new(opts)
   end
   -- toggle the terminal
