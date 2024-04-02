8de07ce5403d291be4434e7ca333b61cfc9c74f9
Micah Halter <micah@mehalter.com>
2024-04-02 13:07:58 -0400

    fix(resession): fix restoration of single tabpages

--- lua/resession/extensions/astrocore.lua
+++ lua/resession/extensions/astrocore.lua
@@ -1,6 +1,7 @@
 local M = {}
 
-M.on_save = function()
+---@param opts resession.Extension.OnSaveOpts
+M.on_save = function(opts)
   -- initiate astronvim data
   local data = { bufnrs = {}, tabs = {} }
 
@@ -11,6 +12,7 @@ M.on_save = function()
 
   -- save tab scoped buffers and buffer numbers from buffer name
   for new_tabpage, tabpage in ipairs(vim.api.nvim_list_tabpages()) do
+    if tabpage == opts.tabpage then data.tabpage = new_tabpage end
     data.tabs[new_tabpage] = vim.t[tabpage].bufs
     for _, bufnr in ipairs(data.tabs[new_tabpage]) do
       data.bufnrs[vim.api.nvim_buf_get_name(bufnr)] = bufnr
@@ -20,26 +22,37 @@ M.on_save = function()
   return data
 end
 
-M.on_load = function(data)
+M.on_post_load = function(data)
   -- create map from old buffer numbers to new buffer numbers
   local new_bufnrs = {}
+  local new_tabpages = vim.api.nvim_list_tabpages()
   for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
     local old_bufnr = data.bufnrs[vim.api.nvim_buf_get_name(bufnr)]
     if old_bufnr then new_bufnrs[old_bufnr] = bufnr end
   end
   -- build new tab scoped buffer lists
-  for tabpage, tabs in pairs(data.tabs) do
-    vim.t[tabpage].bufs = vim.tbl_map(function(bufnr) return new_bufnrs[bufnr] end, tabs)
+  if not data.tabpage then
+    for tabpage, tabs in pairs(data.tabs) do
+      local bufs = vim.tbl_map(function(bufnr) return new_bufnrs[bufnr] end, tabs)
+      vim.t[new_tabpages[tabpage]].bufs = bufs
+    end
+  else
+    vim.t.bufs = vim.tbl_map(function(bufnr) return new_bufnrs[bufnr] end, data.tabs[data.tabpage])
   end
 
   local buf_utils = require "astrocore.buffer"
-  buf_utils.current_buf = new_bufnrs[data.current_buf]
-  buf_utils.last_buf = new_bufnrs[data.last_buf]
+  local current_buf, last_buf = new_bufnrs[data.current_buf], new_bufnrs[data.last_buf]
+  if current_buf and last_buf then
+    buf_utils.current_buf = current_buf
+    buf_utils.last_buf = last_buf
+  end
 
   require("astrocore").event "BufsUpdated"
 
-  if vim.opt.bufhidden:get() == "wipe" and vim.fn.bufnr() ~= buf_utils.current_buf then
-    vim.cmd.b(buf_utils.current_buf)
+  if current_buf and last_buf then
+    if vim.opt.bufhidden:get() == "wipe" and vim.fn.bufnr() ~= buf_utils.current_buf then
+      vim.cmd.b(buf_utils.current_buf)
+    end
   end
 end
 
