0eab12ba359b5fe56fb4e883b62b0f8db30c71ac
Micah Halter <micah@mehalter.com>
2024-09-19 07:50:04 -0400

    ci: add lazy.lua for maintaining `opts_extend` configuration

---
+++ lazy.lua
@@ -0,0 +1,11 @@
+return {
+  "AstroNvim/astrocore",
+  opts_extend = {
+    "rooter.ignore.servers",
+    "rooter.ignore.dirs",
+    "sessions.ignore.buftypes",
+    "sessions.ignore.dirs",
+    "sessions.ignore.filetypes",
+    "git_worktrees",
+  },
+}
